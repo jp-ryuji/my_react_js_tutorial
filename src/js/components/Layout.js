ffba3c44c8c610f77ea8a30676207fc17602ce9b
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-10-12 11:01:50 +0900

    [add] Title

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,9 +3,19 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = {name: "Foo"};
+  }
+
   render() {
+    setTimeout(
+      () => { this.setState({name: "Hello"}); }
+    , 1000);
+
     return (
       <div>
+        {this.state.name}
         <Header />
         <Footer />
       </div>
