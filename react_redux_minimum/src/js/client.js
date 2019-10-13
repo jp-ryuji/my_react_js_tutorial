7062127b8a5216905b03bcebd26663e949a65776
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-10-13 23:34:38 +0900

    [modify] store.subscribe()とstore.dispatch()を追加

--- react_redux_minimum/src/js/client.js
+++ react_redux_minimum/src/js/client.js
@@ -4,4 +4,13 @@ const reducer = () => {
   console.log("reducer has been called.");
 }
 
+// NOTE: createStore() 初期化
 const store = createStore(reducer, 1);
+
+// NOTE: store.subscribe() Storeが変更された時に呼ばれる
+store.subscribe(() => {
+  console.log("store changed", store.getState());
+});
+
+// NOTE: store.dispatch() StoreにAction を送信する
+store.dispatch({type: "INC"});
