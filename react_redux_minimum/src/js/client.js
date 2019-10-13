8722e9def55addf5f8e6c1e0c3f9e26f3574f57c
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-10-14 00:31:24 +0900

    [modify] combinedReducerを使って複数のReducerを扱う

--- react_redux_minimum/src/js/client.js
+++ react_redux_minimum/src/js/client.js
@@ -1,26 +1,22 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const reducer = (state = 0, action) => {
-  switch(action.type) {
-    case "INC":
-      return state + action.payload;
-    case "DEC":
-      return state - action.payload;
-  }
+const userReducer = (state = {}, action) => {
   return state;
 }
 
-// NOTE: createStore() 初期化
-const store = createStore(reducer, 1);
+const tweetsReducer = (state = [], action) => {
+  return state;
+}
 
-// NOTE: store.subscribe() Storeが変更された時に呼ばれる
+const reducers = combineReducers({
+  user: userReducer,
+  tweets: tweetsReducer
+});
+
+const store = createStore(reducers);
+
 store.subscribe(() => {
   console.log("store changed", store.getState());
 });
 
-// NOTE: store.dispatch() StoreにAction を送信する
-store.dispatch({type: "INC", payload: 1});
-store.dispatch({type: "INC", payload: 2});
-store.dispatch({type: "INC", payload: 22});
-store.dispatch({type: "INC", payload: 222});
-store.dispatch({type: "DEC", payload: 1000});
+store.dispatch({type: "FOO", payload: "BAR"});
