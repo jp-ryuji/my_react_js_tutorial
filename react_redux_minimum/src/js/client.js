7978b769a9345509e014bc3e4029feaf4b4b7cca
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-10-14 09:49:26 +0900

    [modify] redux-thunkを組み込みエラーを解消する

--- react_redux_minimum/src/js/client.js
+++ react_redux_minimum/src/js/client.js
@@ -1,11 +1,12 @@
 import { applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
 
 const reducer = (state={}, action) => {
   return state;
 };
 
-const middleware = applyMiddleware(createLogger());
+const middleware = applyMiddleware(thunk, createLogger());
 const store = createStore(reducer, middleware);
 
 store.dispatch((dispatch) => {
