01daf619793d55d4e097d2fbe25accfa52d74059
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-10-14 09:47:54 +0900

    [modify] dispatcherで渡す関数の中にdispatch関数の実体を含める（この時点ではエラー）

--- react_redux_minimum/src/js/client.js
+++ react_redux_minimum/src/js/client.js
@@ -1,10 +1,15 @@
 import { applyMiddleware, createStore } from "redux";
+import { createLogger } from "redux-logger";
 
 const reducer = (state={}, action) => {
   return state;
 };
 
-const middleware = applyMiddleware();
+const middleware = applyMiddleware(createLogger());
 const store = createStore(reducer, middleware);
 
-store.dispatch({type: "FOO"});
+store.dispatch((dispatch) => {
+  dispatch({type: "FOO"});
+  // do something async
+  dispatch({type: "BAR"});
+});
