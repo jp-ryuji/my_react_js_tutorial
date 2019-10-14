73af9a37b06b2ccddd33bdbe750b41088aa2bd34
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-10-14 09:44:27 +0900

    [modify] redux-thunkを使って非同期処理をするためのベースコード

--- react_redux_minimum/src/js/client.js
+++ react_redux_minimum/src/js/client.js
@@ -1,42 +1,10 @@
 import { applyMiddleware, createStore } from "redux";
 
-const reducer = (state = 0, action) => {
-  switch(action.type) {
-    case "INC":
-      state = state + 1;
-      break;
-    case "DEC":
-      state = state - 1;
-      break;
-    case "ERR":
-      throw new Error("It's error!!!!");
-  }
+const reducer = (state={}, action) => {
   return state;
-}
+};
 
-const logger = (store) => (next) => (action) => {
-  console.log("action fired", action);
-  next(action);
-}
+const middleware = applyMiddleware();
+const store = createStore(reducer, middleware);
 
-const error = (store) => (next) => (action) => {
-  try{
-    next(action);
-  } catch (e) {
-    console.log("Error was occured", e);
-  }
-}
-
-const middleware = applyMiddleware(logger, error);
-
-const store = createStore(reducer, 1, middleware);
-
-store.subscribe(() => {
-  console.log("store changed", store.getState());
-});
-
-store.dispatch({type: "INC"});
-store.dispatch({type: "INC"});
-store.dispatch({type: "DEC"});
-store.dispatch({type: "DEC"});
-store.dispatch({type: "ERR"});
+store.dispatch({type: "FOO"});
