5840f8f2cceb8a7798a7c66a2d30ff20dbebd912
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-10-15 10:02:45 +0900

    [modify] トップレベルコンポーネントをRedux コンポーネントでwrap する

--- tweet_app/src/js/client.js
+++ tweet_app/src/js/client.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout />, app);
+ReactDOM.render(
+  <Provider>
+    <Layout />
+  </Provider>, app);
