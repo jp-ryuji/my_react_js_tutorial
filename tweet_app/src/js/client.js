25baf06e2ad898f1b6e3f9e6dd3ec4e727f4c249
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-10-15 10:03:32 +0900

    [modify] StoreとReactを結びつけるためにstoreをimportしてProviderコンポーネントのpropsとして定義を追加する

--- tweet_app/src/js/client.js
+++ tweet_app/src/js/client.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import Layout from "./components/Layout";
+import store from "./store";
 
 const app = document.getElementById('app');
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <Layout />
   </Provider>, app);
