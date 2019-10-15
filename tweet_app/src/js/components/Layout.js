c52e7c7098384d6c3a9db958667d4050b9fb8021
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-10-15 11:10:01 +0900

    [modify] ダミーサーバを立ててtweet一覧を取得する

--- tweet_app/src/js/components/Layout.js
+++ tweet_app/src/js/components/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { fetchUser } from "../actions/userActions";
+import { fetchTweets } from "../actions/tweetsActions";
 
 @connect((store) => {
   return {
@@ -14,7 +15,23 @@ export default class Layout extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchUser());
   }
+  fetchTweets() {
+    this.props.dispatch(fetchTweets());
+  }
   render() {
-    return <h1>{this.props.user.name}</h1>;
+    const { user, tweets } = this.props;
+
+    if (!tweets.length) {
+      return <button onClick={this.fetchTweets.bind(this)}>load tweets</button>;
+    }
+
+    const mappedTweets = tweets.map(tweet => <li key={tweet.id}>{tweet.text}</li>);
+
+    return (
+      <div>
+        <h1>{user.name}</h1>
+        <ul>{mappedTweets}</ul>
+      </div>
+    );
   }
 }
