7ca07bbf707c598f69e02be6a49702e4f846a260
Ryuji Eguchi <eguchi.ryuji@gmail.com>
2019-10-15 10:31:44 +0900

    [modify] @connect decolatorを使ってconnectされたクラスのthis.propsからstateの値を取得する

--- tweet_app/src/js/components/Layout.js
+++ tweet_app/src/js/components/Layout.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 
-@connect()
+import { fetchUser } from "../actions/userActions";
+
+@connect((store) => {
+  return {
+    user: store.userReducer.user,
+    userFetched: store.userReducer.fetched,
+    tweets: store.tweetsReducer.tweets
+  };
+})
 export default class Layout extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchUser());
+  }
   render() {
+    console.log(this.props.user);
+    console.log(this.props.userFetched);
     return null;
   }
 }
